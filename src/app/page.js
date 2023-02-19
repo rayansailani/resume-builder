4fe54c1b1a56886e3b3b3d6df9a1c3032c02e55c
Prajwal Naik <prajwal.naik@hpe.com>
2023-02-19 23:17:21 +0530

    Change button dimensions

--- src/app/page.js
+++ src/app/page.js
@@ -15,7 +15,7 @@ export default function Home() {
         <Image src="/assets/diveresify.png" className='object-contain' width={100} height={100}/>
       </div>
       <div className="flex-grow h-max bg-[#E3EBED] rounded-tr-[40px] rounded-tl-[40px] relative">
-        <div className='flex items-center justify-center absolute h-[100px] w-[100px] rounded-full bottom-[20px] 
+        <div className='flex items-center justify-center absolute p-[20px] rounded-full bottom-[20px] 
           right-[-5%] bg-[#00494D] shadow-[10px_10px_10px_rgba(0,0,0,0.25)] transition-all duration-[700ms] hover:scale-[110%]'>
           <Icon icon="noto:magic-wand" height={60}/>
         </div>
