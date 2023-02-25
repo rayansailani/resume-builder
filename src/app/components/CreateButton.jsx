424b6ea46ccde7fd424362e884ec9bf72c9dae71
Prajwal Naik <prajwal.naik@hpe.com>
2023-02-26 01:12:03 +0530

    Add border to button

--- src/app/components/CreateButton.jsx
+++ src/app/components/CreateButton.jsx
@@ -5,7 +5,7 @@ function CreateButton() {
   return (
     <div className='flex items-center justify-center absolute z-10 p-[20px]
         rounded-full bottom-[20px] right-[-5%] bg-[#00494D] shadow-[10px_10px_10px_rgba(0,0,0,0.25)] 
-        transition-all duration-[700ms] hover:scale-[110%]'>
+        transition-all duration-[700ms] hover:scale-[110%] border-2 border-[#26c0ab]'>
       <Icon icon="noto:magic-wand" height={60}/>
     </div>
   )
