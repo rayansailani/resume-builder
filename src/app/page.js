cb29d07f86910bb45d423f6ca2ebca8f07eb5f44
Mohammed Rayan sailani <rayanbackup1@gmail.com>
2023-02-25 10:45:58 +0530

    hide scrollbar in form container and move create button

--- src/app/page.js
+++ src/app/page.js
@@ -9,17 +9,17 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <div className='flex flex-col h-full m-auto max-w-[64rem] pt-[30px] gap-[20px]'>
+    <div className='flex flex-col h-full m-auto max-w-[64rem] pt-[30px] gap-[20px] relative' >
       <div className="flex items-center justify-center">
         <p className='text-[64px] text-[#00494D] text-center'>diverésify </p>
         <Image src="/assets/diveresify.png" className='object-contain' width={100} height={100}/>
       </div>
-      <div className="flex-grow h-max bg-[#E3EBED] rounded-tr-[40px] rounded-tl-[40px] relative">
-        <div className='flex items-center justify-center absolute p-[20px] rounded-full bottom-[20px] 
+      <div className="form-container flex-grow max-h-full overflow-y-scroll overflow-x-hidden p-5 bg-[#E3EBED] rounded-tr-[40px] rounded-tl-[40px]">
+      </div>
+      <div className='flex items-center justify-center absolute z-10 p-[20px] rounded-full bottom-[20px] 
           right-[-5%] bg-[#00494D] shadow-[10px_10px_10px_rgba(0,0,0,0.25)] transition-all duration-[700ms] hover:scale-[110%]'>
           <Icon icon="noto:magic-wand" height={60}/>
         </div>
-      </div>
     </div>
   )
 }
