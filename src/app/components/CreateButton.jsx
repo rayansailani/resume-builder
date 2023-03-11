ece2dbc8b4a54b51ae2cda13bccd76c71d91453d
Prajwal Naik <prajwal.naik@hpe.com>
2023-03-11 21:58:58 +0530

    Add rotating button border

--- src/app/components/CreateButton.jsx
+++ src/app/components/CreateButton.jsx
@@ -3,11 +3,14 @@ import { Icon } from '@iconify/react'
 
 function CreateButton() {
   return (
-    <div className='flex items-center justify-center absolute z-10 p-[20px]
-        rounded-full bottom-[20px] right-[-5%] bg-[#00494D] shadow-[10px_10px_10px_rgba(0,0,0,0.25)] 
-        transition-all duration-[700ms] hover:scale-[110%] border-2 border-[#26c0ab]'>
-      <Icon icon="noto:magic-wand" height={60}/>
+    <div className="buttonWrapper absolute z-10 bottom-[20px] right-[-5%]">
+      <div className='createButton flex items-center justify-center p-[20px]
+        rounded-full  bg-[#00494D] shadow-[10px_10px_10px_rgba(0,0,0,0.25)] 
+        transition-all duration-[700ms] hover:scale-[110%] overflow-hidden'>
+        <Icon icon="noto:magic-wand" height={60}/>
+      </div>
     </div>
+    
   )
 }
 
